9d1f14486ccd792f5d388190df1f3916caaa6a04
Nate96 <braukhoff96@gmail.com>
2024-02-08 15:31:22 -0600

    add ubuntu stript

---
+++ ubuntu-jade.sh
@@ -0,0 +1,31 @@
+sudo apt update
+
+# installing Python
+sudo apt install python3
+
+# install node
+sudo apt install nodejs
+
+# install lua
+sudo apt install lua5.3
+
+# #install repgrep
+# https://github.com/BurntSushi/ripgrep
+sudo apt-get install ripgrep
+
+# install packer
+git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+
+# install gcc
+sudo apt install build-essentia
+
+# install nvim
+sudo apt install neovim
+
+cd ~/.config
+
+# clone config
+git clone https://github.com/Nate96/jade-setup.git
+
+mkdir ~/jade
+
