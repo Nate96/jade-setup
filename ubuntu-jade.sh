fa2d1ec7fd6ae1b7aa4539168fbc39b71151e353
njbraukh <nbraukhoff@belcon.com>
2024-02-13 12:03:47 -0600

    fixed installing packer for ubuntu

--- ubuntu-jade.sh
+++ ubuntu-jade.sh
@@ -18,7 +18,8 @@ sudo apt install lua5.3
 sudo apt-get install ripgrep
 
 # install packer
-git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # install gcc
 sudo apt install gcc
