1cb17885d71b1994bbd2207b1e8a886924ac454d
Nate96 <braukhoff96@gmail.com>
2026-01-18 15:30:39 -0600

    added confits for tools

--- ubuntu-jade.sh
+++ ubuntu-jade.sh
@@ -69,3 +69,16 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # install tmux
 # https://github.com/tmux/tmux/wiki
 sudo apt install tmux
+
+# lazgit
+# https://github.com/jesseduffield/lazygit
+sudo apt install curl git -y
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" \
+  | grep -Po '"tag_name": "v\K[0-9.]+')
+
+curl -Lo lazygit.tar.gz \
+  "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+
+tar xf lazygit.tar.gz lazygit
+
+sudo install lazygit /usr/local/bin/
